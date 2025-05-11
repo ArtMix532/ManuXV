07fe6f7b37f723ef0280a588c855c90c04541abb
Davi Benjamim Guimarães <154901283+ArtMix532@users.noreply.github.com>
2025-05-11 18:13:52 -0300

    Update CorsConfig.java

--- Back/ManuXV/src/main/java/ManuXV/config/CorsConfig.java
+++ Back/ManuXV/src/main/java/ManuXV/config/CorsConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+
 @Configuration
 public class CorsConfig {
 
@@ -13,11 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://manuxv.netlify.app")
+                        .allowedOriginPatterns("*") // <- trocado
                         .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*");
+                        .allowedHeaders("*")
+                        .allowCredentials(true);    // <- adicionado
             }
         };
     }
 }
-
